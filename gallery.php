411231b3712d23b6c5253f448a2c6647fde276f4
mauriciomac92 <36291153+mauriciomac92@users.noreply.github.com>
2020-03-11 00:03:45 -0700

    finished project

--- gallery.php
+++ gallery.php
@@ -1,42 +1,6 @@
-
-
 <?php
-// since the schools server does not support array_column I wrote it here
-// SORTING HELPER FUNCTION
-if (! function_exists('array_column')) {
-    function array_column(array $input, $columnKey, $indexKey = null) {
-        $array = array();
-        foreach ($input as $value) {
-            if ( !array_key_exists($columnKey, $value)) {
-                trigger_error("Key \"$columnKey\" does not exist in array");
-                return false;
-            }
-            if (is_null($indexKey)) {
-                $array[] = $value[$columnKey];
-            }
-            else {
-                if ( !array_key_exists($indexKey, $value)) {
-                    trigger_error("Key \"$indexKey\" does not exist in array");
-                    return false;
-                }
-                if ( ! is_scalar($value[$indexKey])) {
-                    trigger_error("Key \"$indexKey\" does not contain scalar value");
-                    return false;
-                }
-                $array[$value[$indexKey]] = $value[$columnKey];
-            }
-        }
-        return $array;
-    }
-}
-?>
-
-
-
-<?php
-echo "Current PHP version: ".phpversion();
+/*--------------------------------validation handling code--------------------------*/
 // Checks if input is valid
-
 if(isset($_POST["submit"])) { //if a variable is declared when submit is pressed
     // variables
     $file = $_FILES['fileToUpload'];
@@ -45,107 +9,102 @@ if(isset($_POST["submit"])) { //if a variable is declared when submit is pressed
     $fileError = $_FILES['fileToUpload']['error'];
     $fileType = $_FILES['fileToUpload']['type'];// Gets the ext of file
     $document_root = $_SERVER['DOCUMENT_ROOT'];
+     // all input is trimed and uppercase
+	$getPhotoName = strtoupper(trim($_POST['photoName'])); // input variables 
+	$getDateTaken = $_POST['dateTaken']; // use _POST because its safer
+    $getPhotoGrapher = strtoupper(trim($_POST['photographer']));
+    $getLocation = strtoupper(trim($_POST['location']));
 
-    if($fileError > 0){ //if there is a error then display error sign
-        echo 'Problem: '.$fileError;
+    // All Entries have to be filed
+    if(!$getPhotoName || !$getDateTaken || !$getPhotoGrapher || !$getLocation){
+        echo "<p> You have have to enter all the entries</p>";
         exit;
+    }
+
+    // if input is not uppercase then its a error
+    if(!preg_match("/^[A-Z\d]+$/", $getPhotoName)) {
+        echo "<p>Invalid Photo Name</p>";
+        exit;
+    }
+    if(!preg_match("/^[A-Z\d]+$/", $getPhotoGrapher)) {
+        echo "<p>Invalid PhotoGrapher</p>";
+        exit;
+    }
+    if(!preg_match("/^[A-Z\d]+$/", $getLocation)) {
+        echo "<p>Invalid Location</p>";
+        exit;
+    }
+    //if there is a error then display error sign
+    if($fileError > 0){ 
+        echo 'Problem: ';
+        switch ($fileError) {
+            case 1:
+                echo 'File exceed upload_max_file_size.';
+                break;
+            case 2:
+                echo 'File exceed max_file_size';
+                break;
+            case 3: 
+                echo 'File only partially uploaded.';
+            break;
+            case 4: 
+                echo 'No file uploaded.';
+            break;
+            case 6:
+                echo 'Cannot upload file: No temp directory specified.';
+            break;
+            case 7: 
+                echo 'Upload failed: Cannot write to disk';
+            break;
+            case 8: 
+                echo 'A PHP extension blocked the file upload';
+            break;
+            default:
+                break;
+        }
     } 
     
+    $uploaded_file = 'uploads/'.$fileName;
+
+    if(is_uploaded_file($fileTmpName)){
+        if(!move_uploaded_file($fileTmpName,$uploaded_file)){
+            echo 'Problem: Could not move file to destination directory';
+            exit;
+        }
+    }
+
     // this checks if the file extension is correct
     if($fileType != 'image/jpeg' && $fileType != 'image/png'){
         echo 'Problem: file is not a PNG image or a JPEG: ';
         exit;
     } 
-     $uploaded_file = 'uploads/'.$fileName;
+/* -------------------------------- End Validation --------------------------------- */
+/* --------------------Establish a Database connection and Inserting Data -----------*/
 
-	//Establish parameters for db connection
-	$dbhost = "mariadb";
-	$dbusername = "cs431s28";
-	$dbpassword = "Moh3poox";
+    //Connect to the database and display error msg if can't connect
+    $db = mysqli_connect("localhost", "root", "","Images") //dbHost,bdUsername,dbPassword,dbName
+    or die('Cannot Connect To Database.' . mysqli_connect_error());
 
-	//Connect to the database and apply db parameters
-	$conn = mysql_connect($dbhost, $dbusername, $dbpassword);
-	
-	//Display error msg if can't connect
-	if(!$conn) {
-		die('Could not connect: '.mysql_error());
-	}
-	
-	echo 'Connected successfully';
-    
-	//Select database
-	$db_selected = mysql_select_db("cs431s28", $conn);
-	if(!db_selected) {
-		die ("Can't use cs431s28".mysql_error());
-	}
-	echo "line 81 \n";
-	
-    // all input is trimed and uppercase
-	$getPhotoName = strtoupper(trim($_POST['photoName'])); // input variables 
-	$getDateTaken = trim($_POST['dateTaken']); // use _POST because its safer
-    $getPhotoGrapher = strtoupper(trim($_POST['photographer']));
-    $getLocation = strtoupper(trim($_POST['location']));
+    // echo 'Connected successfully';
 
-    $outputString = $fileName."\t".$getPhotoName."\t".// string to append
-    $getDateTaken."\t".$getPhotoGrapher."\t".$getLocation."\n";
+    //Insert query to DB
+    $insertQuery = "INSERT INTO `Images` 
+    (`fileName`, `name`, `date`, `photographer`, `location`, `image`) 
+    VALUES ('$fileName','$getPhotoName','$getDateTaken','$getPhotoGrapher','$getLocation','$uploaded_file')";
+    // if query works tell user
+    echo (mysqli_query($db,$insertQuery)) ? "" : "</br>image not uploaded";
 
-	echo "\n line 92";
-	//From the cs431s28 db select the Images table and put these variables inside
-	echo "line 94 \n";
-	$store_info_query = "INSERT INTO `Images` (`fileName`, `name`, `date`, `photographer`, `location`, `image`) VALUES('$fileName', '$getPhotoName', '$getDateTaken', '$getPhotoGrapher', '$getLocation', '$uploaded_file')";
-	echo "line 96 \n";
-	
-	echo "line 98";
-	if(mysql_query($store_info_query, $conn)) {
-		echo "New record entered successfully";
-	} else {
-		echo "Error: ".$store_info_query."<br>".$conn->error;
-	}
-	mysql_close($conn);
-	echo "line 105";
-    ?>
-	
-<?php
-    // Read file and add data to array and show pictures.
-	//The purpose of these lines was to store the contents of the 
-	//txt file into an array to manipulate and display output
-    echo "line 111";
-	$bigarray = [];
-	$conn = mysql_connect($dbhost, $dbusername, $dbpassword);
-	
-	//Display error msg if can't connect
-	if(!$conn) {
-		die('Could not connect: '.mysql_error());
-	}
-	
-	echo 'Connected successfully';
-
-	$sql = "SELECT * FROM IMAGES";
-	
-    echo "line 124";
-	$result = $conn->mysql_query($sql);
-	
-	//Not sure what purpose storing in array will serve moving foward
-	//so stopped working on this section for now
-	
-    /* while($rewult->num_rows > 0){
-		while($row = $result->fetch_assoc()){
-			if($lines === false) break; // deletes empty line at the end
-				$line = explode("\t",$lines); // explodes the lines into separate varaibles
-				$tmparray = [$line[0],$line[1],$line[2],$line[3],$line[4]]; // pushing to an array
-        array_push($bigarray,$tmparray);
-		}
-	} */
-
-    //mysql_close($conn);
-
+    // close the database;
+    mysqli_close($db);
+    /* -------------------- End Connectoin and Insert Query ----------------------*/
 }
-
 ?>
+<!------------------------ Added Html And Add Sort Button  ---------------->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gallery</title>
     <link rel="stylesheet" 
     href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
@@ -153,94 +112,75 @@ if(isset($_POST["submit"])) { //if a variable is declared when submit is pressed
 </head>
 <body>
     <header>
-        <h1>View All Photos</h1>   
+        <h1>View All Photos</h1> 
     </header>
-	<!-- Create a form to perform the same thing as index.php and leave it blank-->
 <form action = "gallery.php" method="post" enctype="multipart/form-data">
-    <table> 
-        <tr> 
-            <td> 
-            <div class="form-group">
-                <h2>Sort By:
-                <select id="sortby" class="form-control" name="sort">
-                    <option value="name">Name</option>
-                    <option value="date">Date</option>
-                    <option value="photographer">Photographer</option>
-                    <option value="location">Location</option>
-                </select>
-                <button type="submit" name="ok">Ok</button>
-                </h2>
-            </div>
-            </td>
-	        <form action = "gallery.php" method = "post" enctype = "multipart/form-data">
-            <td> 
-            <!--<input type="button" value="Add another Picture" onClick="javascript:history.go(-1)" />-->
-		    <!-- Go back to the uploads page if the user presses the add another picture button-->
-		    <button type="submit" formaction="$document_root/../index.html"> Add Another Picture</button>
-	        </td>
-	        </form>
-        </tr>
-    </table>
+<table> 
+    <tr> 
+        <td> 
+        <div class="form-group">
+            <h2>Sort By:
+            <select id="sortby" class="form-control" name="sort">
+                <option value="name">Name</option>
+                <option value="date">Date</option>
+                <option value="photographer">Photographer</option>
+                <option value="location">Location</option>
+            </select>
+            <button type="submit" name="ok">Ok</button>
+            </h2>
+        </div>
+        </td>
+        <form action = "gallery.php" method = "post" enctype = "multipart/form-data">
+        <td> 
+        <!--<input type="button" value="Add another Picture" onClick="javascript:history.go(-1)" />-->
+        <!-- Go back to the uploads page if the user presses the add another picture button-->
+        <button type="submit" formaction="$document_root/../index.html"> Add Another Picture</button>
+        </td>
 </form>
-    <div>
+    </tr>
+</table>
+
 <?php
-
-$answer='name';
-//If the user has pressed the ok button for sort....
-//$dbhost = "mariadb";
-//$dbusername = "cs431s28";
-//$dbpassword = "Moh3poox";
-//Connect to the database
-//$conn = mysql_connect($dbhost, $dbusername, $dbpassword);
-echo "line 194";
-$image_data = $mysql->query("FROM cs431s28 SELECT * FROM Images");
-//Display error msg if can't connect
-if(!$conn) {
-	die('Could not connect: '.mysql_error());
-	}
-echo 'Connected successfully';
-
-//This code displays the images in the gallery
-if (isset($_POST["ok"])) {
-//...have gallery.txt be read into $bigarray since the form has refreshed...
-//   Instead of reading from a txt file, we read in from a db
-	 //$fp = fopen("gallery.txt", 'rb');
-	
+/* --------------------Adding A Sort Algorithm And Display Gallery -----------*/
+$answer = 'name';
+if(isset($_POST["ok"])) {
     $answer = $_POST["sort"];
-
-	if($image_data=num_rows > 0) {
-		while($row=$image_data->fetch_assoc()) {
-			echo "fileName: ".$row["fileName"]."name: ".$row["name"]."date: ".$row["date"]."<br>";
-		}
-	}
-	mysql_close($conn);
-
 }
-
-// ...And sort the array according to which "sort" method the user selected in the dropdown
+// default sort
+$sql = "SELECT * FROM Images ORDER BY name";
+// Select sql to query
 if($answer === 'name'){
-    array_multisort( array_column( $bigarray, 1),SORT_ASC,  $bigarray);
+    $sql = "SELECT * FROM Images ORDER BY name";
 } else if($answer === 'date'){
-    array_multisort( array_column( $bigarray, 2),SORT_ASC, SORT_NUMERIC, $bigarray);
+    $sql = "SELECT * FROM Images ORDER BY date";
 } else if($answer === 'photographer'){
-    array_multisort( array_column( $bigarray, 3),SORT_ASC, $bigarray);
+    $sql = "SELECT * FROM Images ORDER BY photographer";
 } else if($answer === 'location'){
-    array_multisort( array_column( $bigarray, 4),SORT_ASC, $bigarray);
+    $sql = "SELECT * FROM Images ORDER BY location";
 }
 
-//Display the gallery by using a for loop and echo data-boxes to the screen
-$len = count($bigarray); // gets bigarray length
-for($row = 0; $row < $len; $row++){
+$db = mysqli_connect("localhost", "root", "","Images") //dbHost,bdUsername,dbPassword,dbName
+or die('Cannot Connect To Database.' . mysqli_connect_error());
+
+// echo "connected successfully";
+
+$result = mysqli_query($db,$sql);
+
+// Display the Images selected 
+while($row = mysqli_fetch_row($result)) {
     echo '<div class="list-content">'; // fileName 
-    echo'<img class="picture-content" src="uploads/'.$bigarray[$row][0].'"/ alt="Error on Displaying"></img>';
-    echo'<div class="data-box">'.$bigarray[$row][1].'</div>'; // name
-    echo'<div class="data-box">'.$bigarray[$row][2].'</div>'; // date
-    echo'<div class="data-box">'.$bigarray[$row][3].'</div>'; // photographer
-    echo'<div class="data-box">'.$bigarray[$row][4].'</div>'; // location
+    echo'<img class="picture-content" src="'.$row[6].'"/ alt="Error on Displaying"></img>';
+    echo'<div class="data-box">'. 'Name: '.$row[2].'</div>'; // name
+    echo'<div class="data-box">'. 'Date: '.$row[3].'</div>'; // date
+    echo'<div class="data-box">'. 'Photographer: '.$row[4].'</div>'; // photographer
+    echo'<div class="data-box">'. 'Location: '.$row[5].'</div>'; // location
     echo'</div>';
 }
+// free memory and close the database;
+mysqli_free_result($result);
+mysqli_close($db);
+/* -----------------------------Ending The Connection-----------------------*/
 ?>
-</div>
-</main>
 </body>
 </html>
+<!-- End Html -->
